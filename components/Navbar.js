d0752a257a8078d223eef5461c9817a8cdcc9320
Shruti Rajput <67470724+ShrutiRajput1234@users.noreply.github.com>
2022-11-20 23:52:14 +0530

    Updated names

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,14 @@
 import Image from 'next/image'
 import avatar from '../temp/avatar.jpg'
-import { BsPerson } from 'react-icons/bs'
-import { useContext } from 'react'
-import { UberContext } from '../context/uberContext'
+import {
+  BsPerson
+} from 'react-icons/bs'
+import {
+  useContext
+} from 'react'
+import {
+  UberContext
+} from '../context/uberContext'
 
 const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
@@ -17,39 +23,89 @@ const style = {
 }
 
 const Navbar = () => {
-  const { currentAccount, connectWallet, currentUser } = useContext(UberContext)
+  const {
+    currentAccount,
+    connectWallet,
+    currentUser
+  } = useContext(UberContext)
 
-  return (
-    <div className={style.wrapper}>
-      <div className={style.leftMenu}>
-        <div className={style.logo}>Uber</div>
-        <div className={style.menuItem}>Ride</div>
-        <div className={style.menuItem}>Drive</div>
-        <div className={style.menuItem}>More</div>
-      </div>
-      <div className={style.rightMenu}>
-        <div className={style.menuItem}>Help</div>
-        <div className={style.menuItem}>{currentUser.name?.split(' ')[0]}</div>
-        <div className={style.userImageContainer}>
-          <Image
-            className={style.userImage}
-            src={avatar}
-            width={40}
-            height={40}
-          />
-        </div>
-        {currentAccount ? (
-          <div>
-            {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
-          </div>
-        ) : (
-          <div className={style.loginButton} onClick={() => connectWallet()}>
-            <BsPerson />
-            <span className={style.loginText}>Log in</span>
-          </div>
-        )}
-      </div>
-    </div>
+  return ( <
+    div className = {
+      style.wrapper
+    } >
+    <
+    div className = {
+      style.leftMenu
+    } >
+    <
+    div className = {
+      style.logo
+    } > CrypTaxi < /div> <
+    div className = {
+      style.menuItem
+    } > Ride < /div> <
+    div className = {
+      style.menuItem
+    } > Drive < /div> <
+    div className = {
+      style.menuItem
+    } > More < /div> <
+    /div> <
+    div className = {
+      style.rightMenu
+    } >
+    <
+    div className = {
+      style.menuItem
+    } > Help < /div> <
+    div className = {
+      style.menuItem
+    } > {
+      currentUser.name ? .split(' ')[0]
+    } < /div> <
+    div className = {
+      style.userImageContainer
+    } >
+    <
+    Image className = {
+      style.userImage
+    }
+    src = {
+      avatar
+    }
+    width = {
+      40
+    }
+    height = {
+      40
+    }
+    /> <
+    /div> {
+      currentAccount ? ( <
+        div > {
+          currentAccount.slice(0, 6)
+        }...{
+          currentAccount.slice(39)
+        } <
+        /div>
+      ) : ( <
+        div className = {
+          style.loginButton
+        }
+        onClick = {
+          () => connectWallet()
+        } >
+        <
+        BsPerson / >
+        <
+        span className = {
+          style.loginText
+        } > Log in < /span> <
+        /div>
+      )
+    } <
+    /div> <
+    /div>
   )
 }
 
